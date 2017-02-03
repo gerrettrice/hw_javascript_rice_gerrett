d500b7aae319247c7e9a2951a2a7a631a43a0139
Gerrett Rice <gerrettrice@gmail.com>
2017-02-03 18:54:18 -0500

    add object president4 to main.js

--- js/main.js
+++ js/main.js
@@ -13,10 +13,19 @@ president2 = {
     party: 'Republican',
     yearsOfPresidency: 8,
 };
+
 president3 = {
     firstName: 'Bill',
     lastName: 'Clinton',
     termLength: 2,
     party: 'Democrat',
     yearsOfPresidency: 8,
+};
+
+president4 = {
+    firstName: 'George',
+    lastName: 'Bush',
+    termLength: 1,
+    party: 'Republican',
+    yearsOfPresidency: 4,
 };
