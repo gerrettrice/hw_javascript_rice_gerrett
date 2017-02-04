aeb8a884137954d9ed5dfb0bef1ab1b056869e8f
Gerrett Rice <gerrettrice@gmail.com>
2017-02-03 19:09:33 -0500

    put president objects into an array

--- js/main.js
+++ js/main.js
@@ -1,39 +1,7 @@
-president1 = {
-    firstName: 'Barack',
-    lastName: 'Obama',
-    termLength: 2,
-    party: 'Democrat',
-    yearsOfPresidency: 8,
-};
-
-president2 = {
-    firstName: 'George',
-    lastName: 'Bush',
-    termLength: 2,
-    party: 'Republican',
-    yearsOfPresidency: 8,
-};
-
-president3 = {
-    firstName: 'Bill',
-    lastName: 'Clinton',
-    termLength: 2,
-    party: 'Democrat',
-    yearsOfPresidency: 8,
-};
-
-president4 = {
-    firstName: 'George',
-    lastName: 'Bush',
-    termLength: 1,
-    party: 'Republican',
-    yearsOfPresidency: 4,
-};
-
-president5 = {
-    firstName: 'Ronald',
-    lastName: 'Reagan',
-    termLength: 2,
-    party: 'Republican',
-    yearsOfPresidency: 8,
-};
+var presidents = [
+    {firstName: 'Barack', lastName: 'Obama', termLength: 2, party: 'Democrat', yearsOfPresidency: 8},
+    {firstName: 'George', lastName: 'Bush', termLength: 2, party: 'Republican', yearsOfPresidency: 8},
+    {firstName: 'Bill', lastName: 'Clinton', termLength: 2, party: 'Democrat', yearsOfPresidency: 8},
+    {firstName: 'George', lastName: 'Bush', termLength: 1, party: 'Republican', yearsOfPresidency: 4},
+    {firstName: 'Ronald', lastName: 'Reagan', termLength: 2, party: 'Republican', yearsOfPresidency: 8}
+];
