2bad1a5eee31831e245e4da4d69e13a4fe619075
Gerrett Rice <gerrettrice@gmail.com>
2017-02-03 20:27:28 -0500

    add function expression assigned to "numba"

--- js/main.js
+++ js/main.js
@@ -3,3 +3,17 @@ function howdy() {
 }
 
 howdy();
+
+var numba = function (string) {
+    if (string.length < 7) {
+        console.log('What a short little word!');
+    } else if (string.length > 7) {
+        console.log('I am sorry, but that is too many to count.');
+    } else {
+        console.log('7, what a perfect choice!');
+    }
+};
+
+numba('7');
+numba('seventy');
+numba('university');
